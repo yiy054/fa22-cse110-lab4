bf96419b0208bf3d3aac026d0e7fc2edbf7e54cf
yi yao <yiy054@ucsd.edu>
2022-10-23 16:05:57 -0700

    jsupdate

---
+++ expose/javascript/part1-question1-2.js
@@ -0,0 +1,8 @@
+function sumValues(num1, num2, add){
+    if(add){
+        var result = 0;
+        console.log('value added: ', result);
+    }else return;
+    console.log('final result: ', result);
+    sumValues(10, 10, true);
+}
