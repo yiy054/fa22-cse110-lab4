151ae3dad5b8c2635308dbe4ef636045b364ee5e
YIZHANG LIU <benjilewiscn@gmail.com>
2022-10-23 16:36:34 -0700

    sendPart1

--- expose/javascript/part1-question1-2.js
+++ expose/javascript/part1-question1-2.js
@@ -1,8 +1,18 @@
 function sumValues(num1, num2, add){
+
     if(add){
-        var result = 0;
+        // var result = 0;
+        // let result = 0;
+        const result = 0;
+    
+        // result = num1 + num2;
+    
         console.log('value added: ', result);
+    
     }else return;
+    
     console.log('final result: ', result);
-    sumValues(10, 10, true);
 }
+
+sumValues(10, 10, true);
+
